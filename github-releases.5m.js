2e7ea07b146302269cd59a071e328dbe4a48652d
Krakaw <41575888+Krakaw@users.noreply.github.com>
2018-12-16 19:10:28 +0200

    Added currentVersion closure

--- github-releases.5m.js
+++ github-releases.5m.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env /usr/local/bin/node
-
 /**
  * <bitbar.title>GitHub Release Version</bitbar.title>
  * <bitbar.version>v1.0</bitbar.version>
@@ -33,7 +32,7 @@ if (!USER_AGENT) {
 
 /**
  * A list of github repos and their name
- * [{url: "https://api.github.com/repos/big-neon/bn-api/tags", name: "big-neon/bn-api" }]
+ * [{url: "https://api.github.com/repos/big-neon/bn-api/tags", name: "big-neon/bn-api", currentVersion: async ():string => {} }]
  * @type {*[{url: string, name: string}]}
  */
 const URLS = [
@@ -44,7 +43,7 @@ const URLS = [
  * The only var is {count} which is the total pending pull requests
  * @type {string}
  */
-const TITLE = "Releases\n---\n";
+const TITLE = "Releases\n---";
 
 const promises = [];
 
@@ -52,16 +51,20 @@ URLS.forEach(url => {
 	promises.push(getContent(url));
 });
 
-Promise.all(promises).then(results => {
+Promise.all(promises).then(async results => {
 	let count = 0;
 	let body = "";
-	results.forEach(result => {
-		result = parseTags(result);
+
+	for (let i in results) {
+		let result = results[i];
+		let current = result.currentVersion ? await result.currentVersion() : false;
+		result = parseTags(result, current);
 		count += result.count;
 		body += result.result;
-	});
+	}
+
 	body = TITLE + "\n" + body;
-	console.log(body);
+	console.log(body.trim());
 }).catch((response) => {
 	if (response.statusCode === 403) {
 		let date = (new Date(response.headers['x-ratelimit-reset'] * 1000));
@@ -77,12 +80,14 @@ Promise.all(promises).then(results => {
  * @param resultData
  * @return {{count: *, result: (string|string)}}
  */
-function parseTags(resultData) {
+function parseTags(resultData, current) {
 	const {jsonString, name} = resultData;
 	let json = JSON.parse(jsonString);
 	let repoName = name;
 	let pull = json.shift();
-	let row = `${repoName}: ${pull.name} | href=https://github.com/big-neon/bn-api/commit/${pull.commit.url.split('/').pop()}\n`;
+	current = current || "No Current Version";
+	let row = `${repoName}: ${pull.name} | href=${pull.commit.url.replace("api.github.com/repos", "github.com").replace("commits/", "commit/")}\n`;
+	row += `${current} | alternate=true\n`;
 	return {result: row};
 
 }
@@ -93,7 +98,7 @@ function parseTags(resultData) {
  * @return {Promise}
  */
 function getContent(contentData) {
-	const {name} = contentData;
+	const {name, currentVersion} = contentData;
 	const url = contentData.url;
 	// return new pending promise
 	return new Promise((resolve, reject) => {
@@ -113,26 +118,38 @@ function getContent(contentData) {
 			// on every content chunk, push it to the data array
 			response.on("data", (chunk) => body.push(chunk));
 			// we are done, resolve promise with those joined chunks
-			response.on("end", () => resolve({jsonString: body.join(""), name}));
+			response.on("end", () => resolve({jsonString: body.join(""), name, currentVersion, response}));
 		});
 		// handle connection errors of the request
 		request.on("error", (err) => reject(err, response))
 	})
 }
 
-function _dateFormat (date, fstr, utc) {
+function _dateFormat(date, fstr, utc) {
 	utc = utc ? 'getUTC' : 'get';
-	return fstr.replace (/%[YmdHMS]/g, function (m) {
+	return fstr.replace(/%[YmdHMS]/g, function (m) {
 		switch (m) {
-			case '%Y': return date[utc + 'FullYear'] (); // no leading zeros required
-			case '%m': m = 1 + date[utc + 'Month'] (); break;
-			case '%d': m = date[utc + 'Date'] (); break;
-			case '%H': m = date[utc + 'Hours'] (); break;
-			case '%M': m = date[utc + 'Minutes'] (); break;
-			case '%S': m = date[utc + 'Seconds'] (); break;
-			default: return m.slice (1); // unknown code, remove %
+			case '%Y':
+				return date[utc + 'FullYear'](); // no leading zeros required
+			case '%m':
+				m = 1 + date[utc + 'Month']();
+				break;
+			case '%d':
+				m = date[utc + 'Date']();
+				break;
+			case '%H':
+				m = date[utc + 'Hours']();
+				break;
+			case '%M':
+				m = date[utc + 'Minutes']();
+				break;
+			case '%S':
+				m = date[utc + 'Seconds']();
+				break;
+			default:
+				return m.slice(1); // unknown code, remove %
 		}
 		// add leading zero if required
-		return ('0' + m).slice (-2);
+		return ('0' + m).slice(-2);
 	});
 }
